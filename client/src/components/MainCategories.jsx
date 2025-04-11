06d014c050b86055b28b8bdd8a9bb4924ffe35b0
mtandao centre <mtandaocentre@gmail.com>
2025-04-11 17:19:56 +0300

    Added Seach component and styling

--- client/src/components/MainCategories.jsx
+++ client/src/components/MainCategories.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import Search from "./Search"
 
 const MainCategories = () => {
   return (
@@ -85,25 +86,10 @@ const MainCategories = () => {
         {/* - Add and style serch bar 
             - Changed stroke color
         */}
-        <div 
-            className="bg-[#e0e0e0] p-2 rounded-full flex items-center 
-            gap-2"
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="20"
-                height="20"
-                fill="none"
-                stroke="#0f0f0f"
-            >
-                <circle cx="10.5" cy="10.5" r="7.5" />
-                <line x1="16.5" y1="16.5" x2="22" y2="22"/>
-            </svg>
-            <input type="text" placeholder="Search" 
-                className="font-medium bg-transparent" 
-            />
+        <div className=" font-normal">
+            <Search />
         </div>
+        
     </div>
   )
 }
