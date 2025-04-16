9396c0a5162480015ea27b8ae8adc15365fc292e
mtandao centre <mtandaocentre@gmail.com>
2025-04-16 22:52:30 +0300

    Added New route to get the logged-in user's full profile

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { getUserSavedPosts, savePost, updateUserDescription } from "../controllers/user.controller.js";
+import { 
+    getCurrentUser, 
+    getUserSavedPosts, 
+    savePost, 
+    updateUserDescription 
+} from "../controllers/user.controller.js";
 
 // define router
 const router = express.Router();
@@ -13,3 +18,6 @@ router.patch("/description", updateUserDescription);
 
 // Export router by default
 export default router
+
+// New route to get the logged-in user's full profile
+router.get("/me", getCurrentUser);
